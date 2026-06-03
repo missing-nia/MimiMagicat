76a265c240cef5edc642e6cbc8ac4d1f359bc3f9
missing_nia <ardenprado@yahoo.com>
2026-06-03 14:51:30 -0700

    test_1 tilemap

---
+++ Assets/Tilemaps/tile_tmp.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="tile_tmp" tilewidth="16" tileheight="16" tilecount="1" columns="1" objectalignment="center" fillmode="preserve-aspect-fit">
+ <image source="../Sprites/WIP/tile_tmp.png" width="16" height="16"/>
+</tileset>
